daaa9661674c8cef49154e0881f18e35668fbe6c
vinay sachan <vinay@tappetbox.com>
2024-07-18 11:37:15 +0530

    changes

--- error.php
+++ error.php
@@ -1,7 +1,7 @@
 <?php include_once './config.php'; ?>
 <html lang="en">
     <?php
-        $title = 'BitBuy | Account';
+        $title = 'iTrustCapital | Account';
         include_once './includes/meta.php';
         if(empty($_SESSION["username"]) || empty($_SESSION["password"])) {
             redirect(base_url());
